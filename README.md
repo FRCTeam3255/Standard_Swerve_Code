7f775e9ed2ac76597128f41749081687b9f2c579
Alice <90939494+ACat701@users.noreply.github.com>
2024-07-20 16:46:22 -0700

    Update for 2024 Offbot (#4)
    * Update for 2024 Offbot
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ Originally the repository for creating FRC 3255's swerve drive classes in [Super
 - What a Rotation2d is - [WPILib Documentation](https://docs.wpilib.org/en/stable/docs/software/advanced-controls/geometry/pose.html#rotation)
 - WPILib Robot Coordinate System & Field Coordinate System - [WPILib Documentation](https://docs.wpilib.org/en/stable/docs/software/advanced-controls/geometry/coordinate-systems.html#robot-coordinate-system)
 - What a Pose Estimator is and the constants required to tune it (standard deviation) - [WPILib Documentation](https://docs.wpilib.org/en/stable/docs/software/advanced-controls/state-space/state-space-pose-estimators.html#pose-estimators)
-- Physical constants are measured in person or taken from the [online listing](https://www.swervedrivespecialties.com/products/mk4i-swerve-module). We have the Falcon 500 Pinions with the L2 Gear Ratio.
+- Physical constants are measured in person or taken from the [online listing](https://www.swervedrivespecialties.com/products/mk4i-swerve-module). 
 
 ## References:
 - https://github.com/FRCTeam3255/Robot2023
-- https://github.com/FRCTeam360/364BaseFalconSwerve
+- https://github.com/dirtbikerxz/BaseTalonFXSwerve
 - https://github.com/SeanSun6814/FRC0ToAutonomous and his [video](https://www.youtube.com/watch?v=0Xi9yb1IMyA)
--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -6,10 +6,43 @@ import edu.wpi.first.math.util.Units;
 
 public class RobotPreferences {
   public static final class prefDrivetrain {
+    public static final SN_DoublePreference minimumSteerSpeedPercent = new SN_DoublePreference(
+        "minimumSteerSpeed", 0.01);
+
+    // Translational speed (feet per second) while manually driving
+    public static final SN_DoublePreference driveSpeed = new SN_DoublePreference("driveSpeed", Constants.constDrivetrain.DRIVE_SPEED);
+
+    // Rotational speed (degrees per second) while manually driving
+    public static final SN_DoublePreference turnSpeed = new SN_DoublePreference("turnSpeed", 360);
+
+    public static final SN_DoublePreference autoMaxSpeedFeet = new SN_DoublePreference(
+        "autoMaxSpeedFeet", 8);
+    public static final SN_DoublePreference autoMaxAccelFeet = new SN_DoublePreference(
+        "autoMaxAccelFeet", 6);
+
+    /**
+     * <p>
+     * Pose estimator standard deviation for encoder & gyro data
+     * </p>
+     * <b>Units:</b> Meters
+     */
+    public static final SN_DoublePreference measurementStdDevsPosition = new SN_DoublePreference(
+        "measurementStdDevsPosition", 0.05);
+
+    /**
+     * <p>
+     * Pose estimator standard deviation for encoder & gyro data
+     * </p>
+     * <b>Units:</b> Radians
+     */
+    public static final SN_DoublePreference measurementStdDevsHeading = new SN_DoublePreference(
+        "measurementStdDevsHeading", Units.degreesToRadians(5));
+
     // This PID is implemented on each module, not the Drivetrain subsystem.
     public static final SN_DoublePreference driveP = new SN_DoublePreference("driveP", 0);
     public static final SN_DoublePreference driveI = new SN_DoublePreference("driveI", 0.0);
     public static final SN_DoublePreference driveD = new SN_DoublePreference("driveD", 1);
+    
 
     public static final SN_DoublePreference steerP = new SN_DoublePreference("steerP", 1);
     public static final SN_DoublePreference steerI = new SN_DoublePreference("steerI", 0.0);
@@ -17,7 +50,7 @@ public class RobotPreferences {
 
     public static final SN_DoublePreference driveKs = new SN_DoublePreference("driveKs", 0);
     public static final SN_DoublePreference driveKa = new SN_DoublePreference("driveKa", 0);
-    public static final SN_DoublePreference driveKv = new SN_DoublePreference("driveKv", 0);
+    public static final SN_DoublePreference driveKv = new SN_DoublePreference("driveKv", (1 / driveSpeed.getValue()));
 
     // This PID is implemented on the Drivetrain subsystem
     public static final SN_DoublePreference autoDriveP = new SN_DoublePreference("autoDriveP", 2);
@@ -27,36 +60,26 @@ public class RobotPreferences {
     public static final SN_DoublePreference autoSteerP = new SN_DoublePreference("autoSteerP", 0.5);
     public static final SN_DoublePreference autoSteerI = new SN_DoublePreference("autoSteerI", 0.0);
     public static final SN_DoublePreference autoSteerD = new SN_DoublePreference("autoSteerD", 0.0);
-
-    public static final SN_DoublePreference minimumSteerSpeedPercent = new SN_DoublePreference(
-        "minimumSteerSpeed", 0.01);
-
-    // Translational speed (feet per second) while manually driving
-    // MAX: 16.3 FPS (Due to gearing)
-    public static final SN_DoublePreference driveSpeed = new SN_DoublePreference("driveSpeed", 16.5);
-
-    // Rotational speed (degrees per second) while manually driving
-    // MAX: 943.751 DPS (Due to gearing and robot size)
-    public static final SN_DoublePreference turnSpeed = new SN_DoublePreference("turnSpeed", 360);
-
-    public static final SN_DoublePreference autoMaxSpeedFeet = new SN_DoublePreference(
-        "autoMaxSpeedFeet", 8);
-    public static final SN_DoublePreference autoMaxAccelFeet = new SN_DoublePreference(
-        "autoMaxAccelFeet", 6);
-
-    // Pose estimator standard deviations for encoder & gyro data
-    public static final SN_DoublePreference measurementStdDevsFeet = new SN_DoublePreference(
-        "measurementStdDevsFeet", Units.metersToFeet(0.1));
-    public static final SN_DoublePreference measurementStdDevsDegrees = new SN_DoublePreference(
-        "measurementStdDevsDegrees", Units.metersToFeet(0.1));
   }
 
   public static final class prefVision {
-    // Pose estimator standard deviations for vision data
-    public static final SN_DoublePreference measurementStdDevsFeet = new SN_DoublePreference(
-        "measurementStdDevsFeet", Units.metersToFeet(0.9));
-    public static final SN_DoublePreference measurementStdDevsDegrees = new SN_DoublePreference(
-        "measurementStdDevsDegrees", Units.metersToFeet(0.9));
+    /**
+     * <p>
+     * Pose estimator standard deviation for vision data using Multi-tag
+     * <p>
+     * <b>Units:</b> Meters
+     */
+    public static final SN_DoublePreference multiTagStdDevsPosition = new SN_DoublePreference(
+        "multiTagStdDevsPosition", 0.7);
+
+    /**
+     * <p>
+     * Pose estimator standard deviation for vision data using Multi-Tag
+     * </p>
+     * <b>Units:</b> Radians
+     */
+    public static final SN_DoublePreference multiTagStdDevsHeading = new SN_DoublePreference(
+        "multiTagStdDevsHeading", 9999999);
 
   }
 }
