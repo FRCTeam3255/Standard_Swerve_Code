21d7181f6795bce8f35376641bda6f46ab6cb564
ACat701 <90939494+ACat701@users.noreply.github.com>
2025-01-10 19:26:23 -0800

    Tested in sim!

--- src/main/java/frc/robot/commands/DriveManual.java
+++ src/main/java/frc/robot/commands/DriveManual.java
@@ -10,12 +10,14 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.constDrivetrain;
+import frc.robot.Constants.constField;
 import frc.robot.subsystems.Drivetrain;
 
 public class DriveManual extends Command {
   Drivetrain subDrivetrain;
   DoubleSupplier xAxis, yAxis, rotationAxis;
   boolean isOpenLoop;
+  double redAllianceMultiplier = 1;
 
   public DriveManual(Drivetrain subDrivetrain, DoubleSupplier xAxis, DoubleSupplier yAxis,
       DoubleSupplier rotationAxis) {
@@ -31,13 +33,16 @@ public class DriveManual extends Command {
 
   @Override
   public void initialize() {
+    redAllianceMultiplier = constField.isRedAlliance() ? -1 : 1;
   }
 
   @Override
   public void execute() {
     // Get Joystick inputs
-    double xVelocity = xAxis.getAsDouble() * constDrivetrain.DRIVE_SPEED.in(Units.MetersPerSecond);
-    double yVelocity = -yAxis.getAsDouble() * constDrivetrain.DRIVE_SPEED.in(Units.MetersPerSecond);
+    double xVelocity = xAxis.getAsDouble() * constDrivetrain.DRIVE_SPEED.in(Units.MetersPerSecond)
+        * redAllianceMultiplier;
+    double yVelocity = -yAxis.getAsDouble() * constDrivetrain.DRIVE_SPEED.in(Units.MetersPerSecond)
+        * redAllianceMultiplier;
     double rVelocity = -rotationAxis.getAsDouble() * constDrivetrain.TURN_SPEED.in(Units.RadiansPerSecond);
 
     subDrivetrain.drive(new Translation2d(xVelocity, yVelocity), rVelocity, isOpenLoop);
