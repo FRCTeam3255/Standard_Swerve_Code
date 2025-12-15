9a416f15c9c143839bc83198ebef638981e0b69c
Yutong Zhu <3681247216@qq.com>
2025-12-14 18:02:09 -0800

    2025 Update V2 ᓚᘏᗢ (#52)
    * Refactor RobotContainer and RobotMap for improved structure; add rotor and motion ID classes
    
    * Rename RobotMap to DeviceIDs and update references

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
-import frc.robot.RobotMap.mapControllers;
+import frc.robot.DeviceIDs.controllerIDs;
 import frc.robot.commands.*;
 import frc.robot.constants.ConstField;
 import frc.robot.constants.ConstSystem.constControllers;
@@ -34,13 +34,14 @@ public class RobotContainer {
 
   private AutoFactory autoFactory;
 
-  private final SN_XboxController conDriver = new SN_XboxController(mapControllers.DRIVER_USB);
+  private final SN_XboxController conDriver = new SN_XboxController(controllerIDs.DRIVER_USB);
 
-  private final Drivetrain subDrivetrain = new Drivetrain();
-  private final DriverStateMachine subDriverStateMachine = new DriverStateMachine(subDrivetrain);
-  private final StateMachine subStateMachine = new StateMachine(subDrivetrain);
-  private final RobotPoses robotPose = new RobotPoses(subDrivetrain);
-  private final Vision subVision = new Vision();
+  public final static Rotors rotorsInstance = new Rotors();
+  public final static Drivetrain subDrivetrain = new Drivetrain();
+  public final static DriverStateMachine subDriverStateMachine = new DriverStateMachine(subDrivetrain);
+  public final static StateMachine subStateMachine = new StateMachine(subDrivetrain);
+  public final static RobotPoses robotPose = new RobotPoses(subDrivetrain);
+  public final static Vision subVision = new Vision();
 
   Command TRY_NONE = Commands.deferredProxy(
       () -> subStateMachine.tryState(RobotState.NONE));
--- src/main/java/frc/robot/subsystems/Rotors.java
+++ src/main/java/frc/robot/subsystems/Rotors.java
@@ -4,11 +4,14 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+@Logged
 public class Rotors extends SubsystemBase {
   /** Creates a new Rotors. */
-  public Rotors() {}
+  public Rotors() {
+  }
 
   @Override
   public void periodic() {
