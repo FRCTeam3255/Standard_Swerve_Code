a81c17658b66bce1876e3147f6caccb90c7db5da
Wu-Fan-529 <3681247216@qq.com>
2026-01-18 13:16:25 +0800

    move ratios to supercore

--- src/main/java/frc/robot/constants/ConstDrivetrain.java
+++ src/main/java/frc/robot/constants/ConstDrivetrain.java
@@ -18,6 +18,8 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants.ClosedLoopOutputType;
+import com.frcteam3255.components.swerve.SN_SuperSwerveV2.ModuleLocations;
+import com.frcteam3255.components.swerve.SN_SuperSwerveV2.Ratios;
 
 import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
@@ -53,55 +55,6 @@ public class ConstDrivetrain {
   public static final Angle FRONT_RIGHT_ABS_ENCODER_OFFSET = Rotations.of(-0.498779296875);
   public static final Angle BACK_LEFT_ABS_ENCODER_OFFSET = Rotations.of(-0.459716796875);
   public static final Angle BACK_RIGHT_ABS_ENCODER_OFFSET = Rotations.of(-0.31201171875);
-
-  // ====== TO MOVE TO SUPERCODE - START ======
-  public static class ModuleLocations {
-    public static final Distance frame25x25 = Inches.of(19.75).div(2);
-    public static final Distance frame29x29 = Inches.of(23.75).div(2);
-  }
-
-  public static class Ratios {
-    private final double steer;
-    private final double drive;
-    private final double couple;
-
-    public Ratios(double steer, double drive, double couple) {
-      this.steer = steer;
-      this.drive = drive;
-      this.couple = couple;
-    }
-
-    public double getSteer() {
-      return steer;
-    }
-
-    public double getDrive() {
-      return drive;
-    }
-
-    public double getCouple() {
-      return couple;
-    }
-
-    public static class MK4I {
-      private static final double stage1 = 1. / (14. / 50.);
-      private static final double stage2L1 = 1. / (25. / 19.);
-      private static final double stage2L2 = 1. / (27. / 17.);
-      private static final double stage2L3 = 1. / (28. / 16.);
-      private static final double stage3 = 1. / (15. / 45.);
-      private static final double steer = 150. / 7.;
-
-      private static final double driveL1 = stage1 * stage2L1 * stage3;
-      private static final double driveL2 = stage1 * stage2L2 * stage3;
-      private static final double driveL3 = stage1 * stage2L3 * stage3;
-      private static final double couple = stage1;
-
-      public static final Ratios L1 = new Ratios(steer, driveL1, couple);
-      public static final Ratios L2 = new Ratios(steer, driveL2, couple);
-      public static final Ratios L3 = new Ratios(steer, driveL3, couple);
-    }
-  }
-  // ====== TO MOVE TO SUPERCODE - END ======
 
   public static final double SLOW_MODE_MULTIPLIER = 0.5;
 
