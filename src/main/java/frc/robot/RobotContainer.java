81fd161aaef037709bbefa213d48d7c70aae7356
Wu-Fan-529 <3681247216@qq.com>
2026-05-17 07:16:26 +0800

    brownout votage

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -83,7 +84,7 @@ public class RobotContainer {
     configDriverBindings();
     configOperatorBindings();
     configAutonomous();
-
+    RobotController.setBrownoutVoltage(5.5);
     // subDrivetrain.resetModulesToAbsolute();
   }
 
