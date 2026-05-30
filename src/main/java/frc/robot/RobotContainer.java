2645760c597dd529ba16cf055acc0839e62178fb
Wu-Fan-529 <3681247216@qq.com>
2026-05-30 09:27:55 +0800

    Add AutoPIDTuning trajectory and update Choreo variables and configurations

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import com.frcteam3255.joystick.SN_XboxController;
 import choreo.auto.AutoFactory;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -22,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import frc.robot.DeviceIDs.controllerIDs;
 import frc.robot.commands.AddVisionMeasurement;
 import frc.robot.commands.ResetPose;
+import frc.robot.constants.ChoreoTraj;
 import frc.robot.constants.ConstSystem;
 import frc.robot.constants.ConstSystem.constControllers;
 import frc.robot.subsystems.DriverStateMachine;
@@ -39,7 +41,9 @@ import frc.robot.subsystems.Vision;
 public class RobotContainer {
   @NotLogged
   SendableChooser<Command> autoChooser = new SendableChooser<>();
-
+  public String pathString = "";
+  public Pose2d pathStartPose = new Pose2d();
+  public Pose2d pathEndPose = new Pose2d();
   private AutoFactory autoFactory;
 
   private final SN_XboxController conDriver = new SN_XboxController(controllerIDs.DRIVER_USB);
@@ -116,33 +120,46 @@ public class RobotContainer {
         driverStateMachineInstance // The drive subsystem
     );
 
+    Command AutoPIDTuning = Commands.sequence(runPath(ChoreoTraj.AutoPIDTuning));
+    Command DoNothing = Commands.none();
+
+    autoChooser.setDefaultOption("Do Nothing", DoNothing);
+    autoChooser.addOption("AutoPIDTuning", AutoPIDTuning);
+
     // make our entries name
-    final Map<Command, String> autoStartingPoses = Map.ofEntries(
-    // Example
-    // Map.entry(autoCommand, "choreoStartingPath"),
-    );
+    final Map<Command, ChoreoTraj> autoStartingPoses = Map.ofEntries(
+        // Example
+        Map.entry(DoNothing, ChoreoTraj.AutoPIDTuning),
+        Map.entry(AutoPIDTuning, ChoreoTraj.AutoPIDTuning));
+
     // enter which we want to do based on name
-    autoChooser.onChange(selectedAuto -> {
-      String startingPose = autoStartingPoses.get(selectedAuto);
-      // if there is a stating pose, reset to it
-      if (startingPose != null) { // Run even when disabled
-        CommandScheduler.getInstance().schedule(autoFactory.resetOdometry(startingPose)
-            .ignoringDisable(true));
+    autoChooser.onChange(selectedAuto ->
+
+    {
+      ChoreoTraj startingPose = autoStartingPoses.get(selectedAuto);
+      // if there is a starting pose, reset to it
+      if (startingPose != null) {
+        autoFactory.resetOdometry(startingPose.name())
+            .ignoringDisable(true) // Run even when disabled
+            .schedule();
       }
     });
 
     // Example: Add autonomous routines to the chooser
-    autoChooser.setDefaultOption("Do Nothing", Commands.none());
-    autoChooser.addOption("Example Path", runPath("ExamplePath"));
     // Add more autonomous routines as needed, e.g.:
     // autoChooser.addOption("Score and Leave", runPath("ScoreAndLeave"));
 
     SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
-  public Command runPath(String pathName) {
-    return autoFactory.trajectoryCmd(pathName).asProxy()
-        .alongWith(Commands.runOnce(() -> driverStateMachineInstance.setDriverState(DriverState.CHOREO)));
+  public Command runPath(ChoreoTraj path) {
+    return autoFactory.trajectoryCmd(path.name()).asProxy()
+        .alongWith(Commands.runOnce(() -> {
+          pathString = path.name();
+          pathStartPose = path.initialPoseBlue();
+          pathEndPose = path.endPoseBlue();
+          driverStateMachineInstance.setDriverState(DriverState.CHOREO);
+        }));
   }
 
   public Command getAutonomousCommand() {
