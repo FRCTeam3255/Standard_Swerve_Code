be85460045f88d90708aa5e3c699ca7aecb15643
Yutong Zhu <3681247216@qq.com>
2026-05-30 11:05:47 -0700

    Standard Autos ⊙.☉ (#102)
    * Add AutoPIDTuning trajectory and update Choreo variables and configurations
    
    * qf

--- src/main/java/frc/robot/constants/ChoreoVars.java
+++ src/main/java/frc/robot/constants/ChoreoVars.java
@@ -1,5 +1,10 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.*;
+
 /**
  * Generated file containing variables defined in Choreo.
  * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
@@ -7,6 +12,12 @@ package frc.robot.constants;
  */
 public final class ChoreoVars {
 
-  private ChoreoVars() {
-  }
+    public static final class Poses {
+        public static final Pose2d pid_end = new Pose2d(3.573, 3.052, Rotation2d.kZero);
+        public static final Pose2d pid_start = new Pose2d(3.573, 6.052, Rotation2d.kZero);
+
+        private Poses() {}
+    }
+
+    private ChoreoVars() {}
 }
